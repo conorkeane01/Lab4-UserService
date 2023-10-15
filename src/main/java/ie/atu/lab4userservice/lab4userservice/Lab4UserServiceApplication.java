1a7cf6ce125790060c6d111a0cfb92761e15ae2d
conorkeane <g00391846@atu.ie>
2023-10-15 15:46:18 +0100

    Created Lab4-UserService project

---
+++ src/main/java/ie/atu/lab4userservice/lab4userservice/Lab4UserServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab4userservice.lab4userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4UserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab4userservice/lab4userservice/Lab4UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab4userservice.lab4userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
